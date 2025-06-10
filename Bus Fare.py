53bc08f5ae9c934c0b6ab258a6f663ddfb9ddf78
Akshaj6 <akshaj.samaria@gmail.com>
2025-06-10 14:51:10 +0530

    88

--- Bus Fare.py
+++ Bus Fare.py
@@ -1,16 +1,15 @@
 class Vehicle:
     def __init__(self, seating_capacity):
         self.seating_capacity = seating_capacity
-    def fare(self):
-        return self.seating_capacity * 100
 class Bus(Vehicle):
     def __init__(self, name, seating_capacity):
-        super().__init__("Bus", seating_capacity)
+        self.name = name
+        super().__init__(seating_capacity)
     def fare(self):
-        bus_fare = super().fare()
+        bus_fare = self.seating_capacity * 100
         maintenance_charge = bus_fare * 0.10
         final_fare = bus_fare + maintenance_charge
         return final_fare
-school_bus = Bus(seating_capacity=50)
+school_bus = Bus("School Bus", 50)
 fare_bus = school_bus.fare()
-print("The final fare for the bus ride is :\n ₹{fare_bus}")
+print("The final fare for the bus ride is :\n ₹", fare_bus)
